8bb2bbd40cc50a5b41ccff0aea030fa7bba48827
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-02 18:20:06 +0530

    Create CommonConstatnts.java

---
+++ online_event_management/src/main/java/com/event/servlets/util/CommonConstatnts.java
@@ -0,0 +1,89 @@
+package com.event.servlets.util;
+
+public class CommonConstatnts {
+
+	/** Constant for config.properties key for query file path */
+	public static final String QUERY_XML = "queryFilePath";
+
+	/** Constant for file path of config.properties */
+	public static final String PROPERTY_FILE = "config.properties";
+
+	/** Constant for query tag in queries.xml */
+	public static final String TAG_NAME = "query";
+
+	/** Constant for query id in queries.xml */
+	public static final String ATTRIB_ID = "id";
+
+	/** Constant for comma */
+	public static final String COMMA = ",";
+
+	/** Constant for url key of MySQL database in config.properties */
+	public static final String URL = "url";
+
+	/** Constant for user name key of MySQL database in config.properties */
+	public static final String USERNAME = "username";
+
+	/** Constant for password key of MySQL database in config.properties */
+	public static final String PASSWORD = "password";
+
+	/** Constant for driver name key of MySQL database in config.properties */
+	public static final String DRIVER_NAME = "driverName";
+
+	/** Constant for Column index one */
+	public static final int COLUMN_INDEX_ONE = 1;
+
+	/** Constant for Column index two */
+	public static final int COLUMN_INDEX_TWO = 2;
+
+	/** Constant for Column index three */
+	public static final int COLUMN_INDEX_THREE = 3;
+
+	/** Constant for Column index four */
+	public static final int COLUMN_INDEX_FOUR = 4;
+
+	/** Constant for Column index five */
+	public static final int COLUMN_INDEX_FIVE = 5;
+
+	/** Constant for Column index six */
+	public static final int COLUMN_INDEX_SIX = 6;
+
+	/** Constant for Column index seven */
+	public static final int COLUMN_INDEX_SEVEN = 7;
+
+	/** Constant for Column index eight */
+	public static final int COLUMN_INDEX_EIGHT = 8;
+
+	/** Constant for Column index nine */
+	public static final int COLUMN_INDEX_NINE = 9;
+
+	/*
+	 * User constants 
+	 */	
+		
+	
+	
+	
+	
+	/*
+	 * Coordinator constants
+	 */
+	
+	
+	
+	
+	
+	 
+	/*
+	 * Company constants
+	 */
+	 
+	
+	
+	
+	
+	
+	 
+	/*
+	 * Admin constants
+	 */
+}
