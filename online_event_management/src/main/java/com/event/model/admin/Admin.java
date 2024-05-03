6bb9c820ecf854597323dfd7552cfd29c3925618
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-04 04:21:47 +0530

    Update Admin.java

---
+++ online_event_management/src/main/java/com/event/model/admin/Admin.java
@@ -0,0 +1,45 @@
+package com.event.model.admin;
+
+public class Admin {
+	private String adminId;
+	private String userName;
+	private String password;
+	/**
+	 * @return the adminId
+	 */
+	public String getAdminId() {
+		return adminId;
+	}
+	/**
+	 * @param adminId the adminId to set
+	 */
+	public void setAdminId(String adminId) {
+		this.adminId = adminId;
+	}
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
