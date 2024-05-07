98e96132b492119c0cd639cacb9e0d819abdf09b
Dewdu Sendanayake <162711007+MeowGranger@users.noreply.github.com>
2024-05-07 18:53:34 +0530

    Create booking.java

---
+++ online_event_management/src/main/java/com/event/model/booking/booking.java
@@ -0,0 +1,49 @@
+package com.event.model.booking;
+
+public class booking 
+{
+	private String booking_id;
+	private String user_name;
+	private String event_name;
+	private String payment_id;
+	private String service_name;
+	private String event_description;
+	
+	public String getBooking_id() {
+		return booking_id;
+	}
+	public void setBooking_id(String booking_id) {
+		this.booking_id = booking_id;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getEvent_name() {
+		return event_name;
+	}
+	public void setEvent_name(String event_name) {
+		this.event_name = event_name;
+	}
+	public String getPayment_id() {
+		return payment_id;
+	}
+	public void setPayment_id(String payment_id) {
+		this.payment_id = payment_id;
+	}
+	public String getService_name() {
+		return service_name;
+	}
+	public void setService_name(String service_name) {
+		this.service_name = service_name;
+	}
+	public String getEvent_description() {
+		return event_description;
+	}
+	public void setEvent_description(String event_description) {
+		this.event_description = event_description;
+	}
+	
+}
