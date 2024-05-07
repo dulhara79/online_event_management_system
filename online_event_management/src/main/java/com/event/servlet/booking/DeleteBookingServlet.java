5b1ce96f6198e02ecd5ddd28dbc0662532c5fc00
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-08 05:15:59 +0530

    Create DeleteBookingServlet.java

---
+++ online_event_management/src/main/java/com/event/servlet/booking/DeleteBookingServlet.java
@@ -0,0 +1,57 @@
+package com.event.servlet.booking;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.event.service.booking.BookingServiceImpl;
+import com.event.service.booking.IBookingService;
+
+/**
+ * Servlet implementation class DeleteBookingServlet
+ */
+@WebServlet("/DeleteBookingServlet")
+public class DeleteBookingServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public DeleteBookingServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		
+		String bookingId = request.getParameter("bookingId");
+		
+		IBookingService ibookingservice = new BookingServiceImpl();
+		ibookingservice.deleteBooking(bookingId);
+		
+		System.out.println("Booking id in deletebooking servlet: " + bookingId);
+		
+		request.setAttribute("bookingId", bookingId);
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/userAllBookings.jsp");
+		dispatcher.forward(request, response);
+	}
+
+}
