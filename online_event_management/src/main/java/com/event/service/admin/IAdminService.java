40a65d9247b2a8442110d3694cfd8b8a11dc95b0
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-11 11:15:52 +0530

    Update IAdminService.java

--- online_event_management/src/main/java/com/event/service/admin/IAdminService.java
+++ online_event_management/src/main/java/com/event/service/admin/IAdminService.java
@@ -1,5 +1,6 @@
 package com.event.service.admin;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 
 import com.event.model.admin.Admin;
@@ -7,6 +8,16 @@ import com.event.model.admin.Admin;
 public interface IAdminService {
 	
 	public static final Logger log = Logger.getLogger(IAdminService.class.getName());
+	
+	public void addAdmin(Admin admin);
+	
+	public ArrayList<Admin> getAdminById(String adminId);
+	
+	public ArrayList<Admin> getAdmins();
+	
+	public void updateAdmin(String adminId, Admin admin);
+	
+	public void deleteAdmin(String adminId);
 	
 	public Admin getAdminByID(String adminId);
 }
