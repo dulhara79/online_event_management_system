27c7f522e6b99207d10ed0405fc292feda56fadd
UVINDUSEN <uvisends24946@gmail.com>
2024-05-12 04:58:24 +0530

    Create ICompanyService.java

---
+++ online_event_management/src/main/java/com/event/service/company/ICompanyService.java
@@ -0,0 +1,25 @@
+
+package com.event.service.company;
+
+
+import java.util.ArrayList;
+
+import com.event.model.company.company;
+
+
+
+public interface ICompanyService 
+{
+	public void addCompany(company company);
+	
+	public ArrayList<company> getcompany(String CompanyId);
+	
+	public ArrayList<company> getcompany();
+	
+	public ArrayList<company> getcompanyById(String CompanyId);
+	
+	public void Updatecompany(String companyId, company company);
+	
+	public void deletecompany(String companyId);
+}
+
