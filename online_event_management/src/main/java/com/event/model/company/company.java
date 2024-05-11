c11c077c8be8a7ae3dc72648031d49173c3b4491
UVINDUSEN <uvisends24946@gmail.com>
2024-05-12 04:58:00 +0530

    Create company.java

---
+++ online_event_management/src/main/java/com/event/model/company/company.java
@@ -0,0 +1,50 @@
+package com.event.model.company;
+
+public class company {
+
+    private String companyId;
+    private String userName;
+    private String password;
+    private String location;
+    private String type;
+    private String numberofemp;
+    
+	public String getCompanyId() {
+		return companyId;
+	}
+	public void setCompanyId(String companyId) {
+		this.companyId = companyId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getNumberofemp() {
+		return numberofemp;
+	}
+	public void setNumberofemp(String string) {
+		this.numberofemp = string;
+	}
+    
+    
+}
