8169500eae5884da8d11b3e9d3c9bdfa39b44509
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-16 01:32:23 +0530

    Update CompanyLoginServlet.java

--- online_event_management/src/main/java/com/event/servlet/login/CompanyLoginServlet.java
+++ online_event_management/src/main/java/com/event/servlet/login/CompanyLoginServlet.java
@@ -11,42 +11,67 @@ import com.event.model.company.company;
 import com.event.service.company.ICompanyService;
 import com.event.service.company.ICompanyServiceImp;
 
+/**
+ * Servlet implementation class AdminLoginServlet
+ */
+@WebServlet("/CompanyLoginServlet")
+public class CompanyLoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 
-//
-// 
-// 
-//@WebServlet("/CompanyLoginServlet")
-//public class CompanyLoginServlet extends HttpServlet {
-//    private static final long serialVersionUID = 1L;
-//
-//    public CompanyLoginServlet() {
-//        super();
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        response.setContentType("text/html");
-//        
-//        String companyId = request.getParameter("companyId");
-//        String password = request.getParameter("password");
-//        
-//        ICompanyService companyService = new ICompanyServiceImp();
-//        company validatedCompany = companyService.getcompanyById(companyId); //getcompanybyid
-//        
-//        if(validatedCompany != null && validatedCompany.getCompanyId() != null &&
-//           validatedCompany.getPassword() != null &&
-//           validatedCompany.getCompanyId().equals(companyId) &&
-//           validatedCompany.getPassword().equals(password)) {
-//            
-//            request.getSession().setAttribute("status", "success");
-//            request.getSession().setAttribute("companyId", validatedCompany.getCompanyId());
-//            request.getSession().setAttribute("companyName", validatedCompany.getCompanyName());
-//            
-//            response.sendRedirect("company.jsp");
-//            
-//        } else {
-//            request.setAttribute("errorMessage", "Incorrect company ID or password. Please try again.");
-//            request.getSession().setAttribute("status", "failed");
-//            request.getRequestDispatcher("/companyLogin.jsp").forward(request, response);
-//        }
-//    }
-//}
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+
+	public CompanyLoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+
+		String companyId = request.getParameter("companyId");
+		String password = request.getParameter("password");
+		
+		System.out.println("Company login servlet id " + companyId);
+		System.out.println("Company login servlet password " + password);
+
+		ICompanyService companyService = new ICompanyServiceImp();
+		company validatedCompany = companyService.getcompanybyId(companyId); // getcompanybyid
+		
+		System.out.println("Company login servlet validatedCompany.getCompanyId() " + validatedCompany.getCompanyId());
+		System.out.println("Company login servlet validatedCompany.getPassword() " + validatedCompany.getPassword());
+
+		if (validatedCompany != null && validatedCompany.getCompanyId() != null
+				&& validatedCompany.getPassword() != null && validatedCompany.getCompanyId().equals(companyId)
+				&& validatedCompany.getPassword().equals(password)) {
+
+			request.getSession().setAttribute("status", "success");
+			request.getSession().setAttribute("companyId", validatedCompany.getCompanyId());
+			request.getSession().setAttribute("companyName", validatedCompany.getUserName());
+
+			response.sendRedirect("companyIndex.jsp");
+
+		} else {
+			request.setAttribute("errorMessage", "Incorrect company ID or password. Please try again.");
+			request.getSession().setAttribute("status", "failed");
+			request.getRequestDispatcher("/companyLogin.jsp").forward(request, response);
+		}
+	}
+}
