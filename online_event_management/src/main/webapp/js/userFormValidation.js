e0801b866b4e8b698baf4969300f2d02bc7d2a03
dulhara kaushalya <dulhara.kaushalya79@gmail.com>
2024-05-16 06:17:12 +0530

    Update userFormValidation.js

--- online_event_management/src/main/webapp/js/userFormValidation.js
+++ online_event_management/src/main/webapp/js/userFormValidation.js
@@ -1,16 +1,69 @@
 /**
  * 
  */
-function validatePhone() {
-	var phone = document.getElementById('phone').value;
-	var regex = /^07\d{8}$/;
-	if (!regex.test(phone)) {
-		document.getElementById('phoneError').innerText = 'Phone number must start with 07 and contain 10 digits.';
+  function validateUserNames() {
+    var user = document.getElementById('username').value;
+    var regex = /^[a-zA-Z]+$/;
+    
+    if (!regex.test(user)) {
+      document.getElementById('usernameError').innerText = 'User name name must contain only alphabetic characters.';
+      return false;
+    } else {
+      document.getElementById('usernameError').innerText = '';
+      return true;
+    }
+}
+
+function validateFirstNames() {
+    var fname = document.getElementById('firstname').value;
+    var regex = /^[a-zA-Z]+$/;
+    
+    if (!regex.test(fname)) {
+      document.getElementById('firstnameError').innerText = 'First name must contain only alphabetic characters.';
+      return false;
+    } else {
+      document.getElementById('firstnameError').innerText = '';
+      return true;
+    }
+}
+
+function validateLastNames() {
+	var lname = document.getElementById('lastname').value;
+    var regex = /^[a-zA-Z]+$/;
+    
+    if (!regex.test(lname)) {
+      document.getElementById('lastnameError').innerText = 'Last name must contain only alphabetic characters.';
+      return false;
+    } else {
+      document.getElementById('lastnameError').innerText = '';
+      return true;
+    }
+ }
+ 
+ function validateAddress() {
+    var address = document.getElementById('address').value;
+    var regex = /^[a-zA-Z0-9/. ,]+$/;
+
+    if (!regex.test(address)) {
+      document.getElementById('addressError').innerText = 'Address can only contain letters, numbers, forward slashes, dots, and commas.';
+      return false;
+    } else {
+      document.getElementById('addressError').innerText = '';
+      return true;
+    }
+  }
+ 
+ function validatePhone() {
+	var phone = document.getElementById('mobileno').value;
+	var num = /^07\d{8}$/;
+	
+	if (!num.test(phone)) {
+		document.getElementById('phoneError').innerText = '* Phone number must start with 07 and contain 10 digits.';
 		return false;
 	} else {
-		document.getElementById('phoneError').innerText = '';
-		return true;
-	}
+      document.getElementById('phoneError').innerText = '';
+      return true;
+    }
 }
 
 function validatePasswords() {
@@ -28,5 +81,9 @@ function validatePasswords() {
 function enableSubmit() {
 	var isPhoneValid = validatePhone();
 	var arePasswordsValid = validatePasswords();
-	document.getElementById('submit').disabled = !(isPhoneValid && arePasswordsValid);
+	var isFirstnameValid = validateFirstNames();
+	var isLastnameValid = validateLastNames();
+	var isAddressValid = validateAddress();
+	
+	document.getElementById('submit').disabled = !(isPhoneValid && arePasswordsValid && isFirstnameValid && isLastnameValid && isAddressValid);
 }
